021ceece5b27f3c9746ef566bc15d8a852da0a27
James Yoo <jyoo980@gmail.com>
2019-06-30 14:36:52 -0700

    Updating csv write function

--- util/fs_util.py
+++ util/fs_util.py
@@ -1,10 +1,10 @@
-from typing import List
+from typing import *
 import csv
 import os
 
 DATA_FOLDER_NAME = "data"
 
-def write_to_csv(data: List[List[int]], filename: str) -> None:
+def write_to_csv(data: List[List[Any]], filename: str) -> None:
     """Writes the given data to a csv file
 
     Args:
@@ -15,13 +15,14 @@ def write_to_csv(data: List[List[int]], filename: str) -> None:
     Returns:
         None
     """
-    file_path: str = _get_path(filename)
-    with open(file_path, "w") as csv_file:
-        writer = csv.writer(csv_file)
-        for row in data:
-            writer.writerow(row)
+    if data:
+        file_path: str = _get_path(filename)
+        with open(file_path, "w") as csv_file:
+            writer = csv.writer(csv_file)
+            for row in data:
+                writer.writerow(row)
 
-    csv_file.close()
+        csv_file.close()
 
 def _get_path(filename) -> str:
     return os.path.join("..", DATA_FOLDER_NAME, filename)
