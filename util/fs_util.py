cce0d5273a1f4673f80dd8099d0b434ef79af7b9
James Yoo <jyoo980@gmail.com>
2019-06-30 15:46:21 -0700

    Whitespace errors

--- util/fs_util.py
+++ util/fs_util.py
@@ -25,12 +25,12 @@ def write_to_csv(data: List[List[Any]], filename: str) -> None:
 
 def get_path(filename: str) -> str:
     """Produces a file path used in write operations
-	
-	Args:
-		filename: the name of the file to save data to, does not need to 
-			already exist in the file system.
-
-	Returns:
-		a string in the format "../DATA_FOLDER_NAME/<filename>"	
-	"""
-	return os.path.join("..", DATA_FOLDER_NAME, filename)
+    
+    Args:
+        filename: the name of the file to save data to, does not need to
+        already exist in the file system.
+    
+    Returns:
+        a string in the format "../DATA_FOLDER_NAME/<filename>"
+    """
+    return os.path.join("..", DATA_FOLDER_NAME, filename)
