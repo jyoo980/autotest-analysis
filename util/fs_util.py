368ded4ccbf140993c6165fe011dc69e099763ec
James Yoo <jyoo980@gmail.com>
2019-06-30 10:09:03 -0700

    Adding utility functions for filesystem manipulation

---
+++ util/fs_util.py
@@ -0,0 +1,27 @@
+from typing import List
+import csv
+import os
+
+DATA_FOLDER_NAME = "data"
+
+def write_to_csv(data: List[List[int]], filename: str) -> None:
+    """Writes the given data to a csv file
+
+    Args:
+        data: the contents of the csv file to be created. data[0] will act as the
+            labels for the csv file.
+        filename: the name of the csv file to create
+
+    Returns:
+        None
+    """
+    file_path: str = _get_path(filename)
+    with open(file_path, "w") as csv_file:
+        writer = csv.writer(csv_file)
+        for row in data:
+            writer.writerow(row)
+
+    csv_file.close()
+
+def _get_path(filename) -> str:
+    return os.path.join("..", DATA_FOLDER_NAME, filename)
