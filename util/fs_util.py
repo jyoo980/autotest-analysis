ddc6410eebfdce92ad92d475bb8399320a7df638
James Yoo <jyoo980@gmail.com>
2019-06-30 15:43:51 -0700

    Adding documentation + type annotation for get_path in fs_util.py

--- util/fs_util.py
+++ util/fs_util.py
@@ -23,5 +23,14 @@ def write_to_csv(data: List[List[Any]], filename: str) -> None:
 
         csv_file.close()
 
-def get_path(filename) -> str:
-    return os.path.join("..", DATA_FOLDER_NAME, filename)
+def get_path(filename: str) -> str:
+    """Produces a file path used in write operations
+	
+	Args:
+		filename: the name of the file to save data to, does not need to 
+			already exist in the file system.
+
+	Returns:
+		a string in the format "../DATA_FOLDER_NAME/<filename>"	
+	"""
+	return os.path.join("..", DATA_FOLDER_NAME, filename)
