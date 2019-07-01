85a212f69d39e9a57166840bad99b048864e56cd
James Yoo <jyoo980@gmail.com>
2019-07-01 13:50:11 -0700

    Adding functions for parsing csids from CPSC 210 commit urls

--- test/test_deliv_util.py
+++ test/test_deliv_util.py
@@ -12,3 +12,11 @@ def test_parse_csid_empty_url():
 def test_parse_csid_valid_url():
     some_url: str = "https://github.ugrad.cs.ubc.ca/CPSC310-2018W-T2/project_l4k0b_r2l9b/commit/6785aed12ca2181f458a074a35d6b72ea5739630"
     assert parse_csid(some_url) == ("l4k0b", "r2l9b")
+
+def test_parse_csid_210_url():
+    url_210: str = "https://github.ugrad.cs.ubc.ca/CPSC210-2018W-T2/PomoTODO_phase4_l4k0b/commit/9f496ae3ceefc50cb4d04e2b633fc74ed86f034d"
+    assert parse_csid(url_210) == ("l4k0b", None)
+
+def test_parse_csid_210_fails_gracefully():
+    invalid_210_url: str = "https://github.ugrad.cs.ubc.ca/CPSC210-2018W-T2/PomoTODO_/commit/9f496ae3ceefc50cb4d04e2b633fc74ed86f034d"
+    assert parse_csid(invalid_210_url) == (None, None)
--- util/deliv_util.py
+++ util/deliv_util.py
@@ -1,6 +1,11 @@
 from typing import Tuple, Optional
 import re
 
+REGEXP_310: str = "_[a-z0-9]*_[a-z0-9]*"
+REGEXP_210: str = "[a-z][0-9][a-z][0-9][a-z]*/"
+DELIM_310: str = "_"
+DELIM_210: str = "/"
+
 def parse_csid(url: str) -> Tuple[Optional[str], Optional[str]]:
     """Parses a tuple of csids from a given commit url. As long as the given commit url contains
     the string "project", and has a sequence of csids which match the regexp: _[a-z0-9]*_[a-z0-9]*
@@ -10,11 +15,15 @@ def parse_csid(url: str) -> Tuple[Optional[str], Optional[str]]:
         url: the commit url from which a tuple of csids are to be extracted from
 
     Returns:
-        A tuple containing at most two csids. Note that both these fields are optional.    
+        A tuple containing at most two csids. Note that both these fields are optional.
     """
-    if not str or "project" not in url:
-        return (None, None)
+    if _contains_match(url):
+        pattern, delim = (REGEXP_310, DELIM_310) if "project" in url else (REGEXP_210, DELIM_210)
+        ids_match = re.search(pattern, url)
+        ids: List[str] = ids_match.group(0).split(delim)
+        return (ids[0], None) if pattern == REGEXP_210 else (ids[1], ids[2])
     else:
-        ids_match = re.search("_[a-z0-9]*_[a-z0-9]*", url)
-        ids = ids_match.group(0).split("_")
-        return (ids[1], ids[2])
+        return (None, None)
+
+def _contains_match(url: str) -> bool:
+    return url and re.search(REGEXP_310, url) or re.search(REGEXP_210, url)
