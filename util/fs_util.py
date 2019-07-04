2e1a5686c50bb6d27c13a27724424efc4b7a215c
James Yoo <jyoo980@gmail.com>
2019-07-03 17:52:59 -0700

    Adding new path string function which specifies directory

--- util/fs_util.py
+++ util/fs_util.py
@@ -15,3 +15,14 @@ def get_path(filename: str) -> str:
         a string in the format "../DATA_FOLDER_NAME/<filename>"
     """
     return os.path.join("..", DATA_FOLDER_NAME, filename)
+
+def get_path_dir(dir: str, filename: str) -> str:
+    """Produces a file path used in write operations with the specified
+    directory
+
+    Args:
+        dir: the name of the directory to save data to
+        filename: the name of the file to save data to, does not need
+        to already exist in the file system
+    """
+    return os.path.join("..", dir, filename)
